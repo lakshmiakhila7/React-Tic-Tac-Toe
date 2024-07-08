2e148697f389fa2a84ac31a2bbe23f1941480410
lakshmiakhila7 <lakshmiakhila3031@gmail.com>
2024-07-08 14:37:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Game from './components/App';
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
